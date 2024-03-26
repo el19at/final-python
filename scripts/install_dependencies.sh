cc0028ec714bae5422ef8be51142cd12a3cd39be
Elya Athlan <84102911+el19at@users.noreply.github.com>
2024-03-26 19:49:50 +0200

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 apt update
 apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev wget libbz2-dev
 wget https://www.python.org/ftp/python/3.7.4/Python-3.7.4.tgz

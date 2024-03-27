e7633e65b0b7d7c150661851ec1df67b5bb441c8
Elya Athlan <84102911+el19at@users.noreply.github.com>
2024-03-27 17:36:20 +0200

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh ./get-docker.sh
-git clone https://github.com/el19at/final-python.git
-cd final-python
 sudo docker build . py_server

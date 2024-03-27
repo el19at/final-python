eb1864c39cb0d2d3f40dee85f5c0318b639e060b
Elya Athlan <84102911+el19at@users.noreply.github.com>
2024-03-27 19:20:01 +0200

    Update stop_app.sh

--- scripts/stop_app.sh
+++ scripts/stop_app.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
+echo hello
 
-# Log file location
-LOG_FILE="/var/log/stop_app.log"
+# # Log file location
+# LOG_FILE="/var/log/stop_app.log"
 
-# Redirect stdout and stderr to log file
-exec > >(tee -a $LOG_FILE)
-exec 2>&1
+# # Redirect stdout and stderr to log file
+# exec > >(tee -a $LOG_FILE)
+# exec 2>&1
 
-# Log start of script
-echo "[$(date)] Starting stop_app.sh script..."
+# # Log start of script
+# echo "[$(date)] Starting stop_app.sh script..."
 
-# Stop the Docker container
-echo "[$(date)] Stopping py_server Docker container..."
-sudo docker stop py_server
+# # Stop the Docker container
+# echo "[$(date)] Stopping py_server Docker container..."
+# sudo docker stop py_server
 
-# Check exit status of Docker stop command
-if [ $? -eq 0 ]; then
-    echo "[$(date)] py_server Docker container stopped successfully."
-else
-    echo "[$(date)] Error: Failed to stop py_server Docker container." >&2
-fi
+# # Check exit status of Docker stop command
+# if [ $? -eq 0 ]; then
+#     echo "[$(date)] py_server Docker container stopped successfully."
+# else
+#     echo "[$(date)] Error: Failed to stop py_server Docker container." >&2
+# fi
 
-# Log end of script
-echo "[$(date)] stop_app.sh script execution completed."
+# # Log end of script
+# echo "[$(date)] stop_app.sh script execution completed."
